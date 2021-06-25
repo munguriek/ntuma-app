0351ef2e1a9211136b9899c82df794493a2aad22
munguriek <benjaminmunguriek@gmail.com>
2021-06-25 11:06:28 +0300

    Pointed to remote server

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -1,8 +1,8 @@
 import { Platform } from 'react-native'
 
 
-// let baseURL = 'https://ntuma-server.herokuapp.com/api/v1/';
-let baseURL = '';
+let baseURL = 'https://ntuma-server.herokuapp.com/api/v1/';
+//let baseURL = '';
 
 {Platform.OS == 'android'
 ? baseURL = 'http://10.0.2.2:3000/api/v1/'
